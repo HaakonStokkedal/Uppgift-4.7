3a688f898da57753d026d2ddb30c4682abee314d
hast06001 <hast06001@E-PW024NW1>
2023-10-19 21:36:05 +0200

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande.");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur många steg ska varje bokstav stå från vänster.");
+            int steg = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < meddelande.Length; i++)
+            {
+                for(int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
